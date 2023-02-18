cb1b114f13951872d59e377222e0cefc20e52cd6
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-18 13:08:33 +0100

    add function & reference to HTML

--- client/app.js
+++ client/app.js
@@ -16,4 +16,52 @@ const userNameInput = document.querySelector('#username');
 // Reference to the text field for the message content input
 const messageContentInput = document.querySelector('#message-content');
 
-let userName;
+let userName = '';
+
+const login = (e) => {
+  e.preventDefault();
+  if (userNameInput.value === '') {
+    alert('Please enter your name.');
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  }
+};
+
+const sendMessage = (event) => {
+  event.preventDefault();
+
+  const messageContent = messageContentInput.value;
+
+  if (messageContent === '') {
+    alert('Message content cannot be empty!');
+    return;
+  }
+
+  addMessage(userName, messageContent);
+  messageContentInput.value = '';
+};
+
+const addMessage = (author, content) => {
+  // create li element
+  const message = document.createElement('li');
+
+  // add classes
+  message.classList.add('message', 'message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+
+  // create h3&div element for author&content
+  message.innerHTML = `
+    <h3 class="message__author">${author === userName ? 'You' : author}</h3>
+    <div class="message__content">${content}</div>
+  `;
+
+  // append message li to message list
+  messagesList.appendChild(message);
+}
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener("submit", (e) => sendMessage(e));
