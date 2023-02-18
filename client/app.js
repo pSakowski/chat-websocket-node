d5cef189eb2996b6d9a7e6d19044934bc66fe287
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-18 11:53:29 +0100

    init app & add reference to HTML

---
+++ client/app.js
@@ -0,0 +1,19 @@
+// Reference to the login form
+const loginForm = document.querySelector('#welcome-form');
+
+// Reference to the messages section
+const messagesSection = document.querySelector('#messages-section');
+
+// Reference to the message list
+const messagesList = document.querySelector('#messages-list');
+
+// Reference to the form for adding a message
+const addMessageForm = document.querySelector('#add-messages-form');
+
+// Reference to the text field for the username input
+const userNameInput = document.querySelector('#username');
+
+// Reference to the text field for the message content input
+const messageContentInput = document.querySelector('#message-content');
+
+let userName;
