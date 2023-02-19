c070a64eb095aa2e302b0bc99f9cb27e950e9043
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-19 13:54:35 +0100

    add handle new connection to server

--- client/app.js
+++ client/app.js
@@ -1,40 +1,52 @@
+// Create a socket instance for the client to communicate with the server
 const socket = io();
 
+// Listen for the 'message' event, which the server will emit whenever a new message is received
 socket.on('message', ({ author, content }) => {
+  // Call the addMessage function to add the new message to the message list
   addMessage(author, content);
 });
 
-// Reference to the login form
+// Listen for the 'user joined' event, which the server will emit when a new user joins the chat
+socket.on('user joined', (user) => {
+  // Call the addMessage function to add a notification that a new user has joined the chat
+  addMessage('Chat Bot', `User-${user.name} has joined the conversation!`);
+});
+
+// Listen for the 'user left' event, which the server will emit when a user leaves the chat
+socket.on('user left', (user) => {
+  // Call the addMessage function to add a notification that a user has left the chat
+  addMessage('Chat Bot', `User-${user.name} has left the conversation.`);
+});
+
+// Get references to HTML elements
 const loginForm = document.querySelector('#welcome-form');
-
-// Reference to the messages section
 const messagesSection = document.querySelector('#messages-section');
-
-// Reference to the message list
 const messagesList = document.querySelector('#messages-list');
-
-// Reference to the form for adding a message
 const addMessageForm = document.querySelector('#add-messages-form');
-
-// Reference to the text field for the username input
 const userNameInput = document.querySelector('#username');
-
-// Reference to the text field for the message content input
 const messageContentInput = document.querySelector('#message-content');
 
+// Initialize userName variable
 let userName = '';
 
+// Define login function
 const login = (e) => {
   e.preventDefault();
   if (userNameInput.value === '') {
     alert('Please enter your name.');
   } else {
+    // Store the user's name in the userName variable
     userName = userNameInput.value;
+    // Emit a 'join' event to the server with the user's name
+    socket.emit('join', userName);
+    // Hide the login form and show the messages section
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
   }
 };
 
+// Define sendMessage function
 const sendMessage = (e) => {
   e.preventDefault();
 
@@ -45,30 +57,37 @@ const sendMessage = (e) => {
     return;
   }
 
+  // Call the addMessage function to add the user's message to the message list
   addMessage(userName, messageContent);
-  socket.emit('message', { author: 'John Doe', content: 'Lorem Ipsum' });
+  // Emit a 'message' event to the server with the user's name and message content
+  socket.emit('message', { author: userName, content: messageContent });
+  // Clear the message input field
   messageContentInput.value = '';
 };
 
-const addMessage = (author, content) => {
-  // create li element
+// Define addMessage function
+const addMessage = (author, content, style = '') => {
+  // Create a new li element to hold the message
   const message = document.createElement('li');
 
-  // add classes
+  // Add the 'message' and 'message--received' classes to the message element
   message.classList.add('message', 'message--received');
+
+  // If the message is from the current user, add the 'message--self' class
   if (author === userName) {
     message.classList.add('message--self');
   }
 
-  // create h3&div element for author&content
+  // Set the innerHTML of the message element to include the author and content of the message
   message.innerHTML = `
     <h3 class="message__author">${author === userName ? 'You' : author}</h3>
-    <div class="message__content">${content}</div>
+    <div class="message__content" style="font-style: ${style}">${content}</div>
   `;
 
-  // append message li to message list
+  // Append the message element to the message list
   messagesList.appendChild(message);
-}
+};
 
+// Add event listeners for the login and send message
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
--- server.js
+++ server.js
@@ -20,3 +20,44 @@ const server = app.listen(port, () => {
 
 // Set up Socket.IO
 const io = socket(server);
+
+// Initialize the users array
+let users = [];
+
+// Handle a new connection to the server
+io.on('connection', (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+  
+  // Handle a new user joining the chat
+  socket.on('join', (name) => {
+    const user = { name, id: socket.id };
+    users.push(user);
+    
+    // Notify all clients that a new user has joined the chat
+    const message = { author: 'Chat Bot', content: `<em>User-${name} has joined the conversation!</em>` };
+    socket.broadcast.emit('message', message);
+    console.log(`User ${name} connected`);
+  });
+
+  // Handle a new message from the client
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message.content}`);
+
+    // Broadcast the message to all clients except the sender
+    socket.broadcast.emit('message', message);
+  });
+
+  // Handle disconnection from the client
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected: ${socket.id}`);
+
+    // Remove the user from the list of connected users
+    const user = users.find((user) => user.id === socket.id);
+    if (user) {
+      users = users.filter((user) => user.id !== socket.id);
+      const message = { author: 'Chat Bot', content: `User-${user.name} has left the conversation!` };
+      socket.broadcast.emit('message', message);
+      console.log(`User ${user.name} disconnected`);
+    }
+  });
+});
